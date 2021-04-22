c3640feae6c15f10a30dec5ca36eab6a0342a7fd
Taylor Masiello <masiellotay@gmail.com>
2021-04-22 09:46:22 -0700

    Alert

--- components/FavoritesComponent.js
+++ components/FavoritesComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { FlatList, View, Text, StyleSheet } from "react-native";
+import { FlatList, View, Text, StyleSheet, Alert } from "react-native";
 import { ListItem } from "react-native-elements";
 import { connect } from "react-redux";
 import { Loading } from "./LoadingComponent";
@@ -32,7 +32,26 @@ class Favorites extends Component {
           <View style={styles.deleteView}>
             <TouchableOpacity
               style={styles.deleteTouchable}
-              onPress={() => this.props.deleteFavorite(item.id)}
+              onPress={() =>
+                Alert.alert(
+                  "Delete Favorite?",
+                  "Are you sure you wish to delete the favorite campsite " +
+                    item.name +
+                    "?",
+                  [
+                    {
+                      text: "Cancel",
+                      onPress: () => console.log(item.name + "Not Deleted"),
+                      style: "cancel",
+                    },
+                    {
+                      text: "OK",
+                      onPress: () => this.props.deleteFavorite(item.id),
+                    },
+                  ],
+                  { cancelable: false }
+                )
+              }
             >
               <Text style={styles.deleteText}>Delete</Text>
             </TouchableOpacity>
