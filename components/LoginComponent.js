72b53515cd1a29a624171c4314fb8c516475d64c
Taylor Masiello <masiellotay@gmail.com>
2021-04-29 15:16:47 -0700

    Secure Store

---
+++ components/LoginComponent.js
@@ -0,0 +1,107 @@
+import React, { Component } from "react";
+import { View, Button, StyleSheet } from "react-native";
+import { Input, CheckBox } from "react-native-elements";
+import * as SecureStore from "expo-secure-store";
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+      password: "",
+      remember: false,
+    };
+  }
+
+  static navigationOptions = {
+    title: "Login",
+  };
+
+  handleLogin() {
+    console.log(JSON.stringify(this.state));
+    if (this.state.remember) {
+      SecureStore.setItemAsync(
+        "userinfo",
+        JSON.stringify({
+          username: this.state.username,
+          password: this.state.password,
+        })
+      ).catch((error) => console.log("Could not save user info", error));
+    } else {
+      SecureStore.deleteItemAsync("userinfo").catch((error) =>
+        console.log("Could not delete user info", error)
+      );
+    }
+  }
+
+  componentDidMount() {
+    SecureStore.getItemAsync("userinfo").then((userdata) => {
+      const userinfo = JSON.parse(userdata);
+      if (userinfo) {
+        this.setState({ username: userinfo.username });
+        this.setState({ password: userinfo.password });
+        this.setState({ remember: true });
+      }
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Input
+          placeholder="Username"
+          leftIcon={{ type: "font-awesome", name: "user-o" }}
+          onChangeText={(username) => this.setState({ username })}
+          value={this.state.username}
+          containerStyle={styles.formInput}
+          leftIconContainerStyle={styles.formIcon}
+        />
+        <Input
+          placeholder="Password"
+          leftIcon={{ type: "font-awesome", name: "key" }}
+          onChangeText={(password) => this.setState({ password })}
+          value={this.state.password}
+          containerStyle={styles.formInput}
+          leftIconContainerStyle={styles.formIcon}
+        />
+        <CheckBox
+          title="Remember Me"
+          center
+          checked={this.state.remember}
+          onPress={() => this.setState({ remember: !this.state.remember })}
+          containerStyle={styles.formCheckbox}
+        />
+        <View style={styles.formButton}>
+          <Button
+            onPress={() => this.handleLogin()}
+            title="Login"
+            color="#5637DD"
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    margin: 20,
+  },
+  formIcon: {
+    marginRight: 10,
+  },
+  formInput: {
+    padding: 10,
+  },
+  formCheckbox: {
+    margin: 10,
+    backgroundColor: null,
+  },
+  formButton: {
+    margin: 40,
+  },
+});
+
+export default Login;
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -6,6 +6,7 @@ import Directory from "./DirectoryComponent";
 import CampsiteInfo from "./CampsiteInfoComponent";
 import Reservation from "./ReservationComponent";
 import Favorites from "./FavoritesComponent";
+import Login from "./LoginComponent";
 import {
   View,
   Platform,
@@ -190,6 +191,31 @@ const FavoritesNavigator = createStackNavigator(
   }
 );
 
+const LoginNavigator = createStackNavigator(
+  {
+    Login: { screen: Login },
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      headerStyle: {
+        backgroundColor: "#5637DD",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
+      headerLeft: (
+        <Icon
+          name="sign-in"
+          type="font-awesome"
+          iconStyle={styles.stackIcon}
+          onPress={() => navigation.toggleDrawer()}
+        />
+      ),
+    }),
+  }
+);
+
 const CustomDrawerContentComponent = (props) => (
   <ScrollView>
     <SafeAreaView
@@ -214,6 +240,19 @@ const CustomDrawerContentComponent = (props) => (
 
 const MainNavigator = createDrawerNavigator(
   {
+    Login: {
+      screen: LoginNavigator,
+      navigationOptions: {
+        drawerIcon: ({ tintColor }) => (
+          <Icon
+            name="sign-in"
+            type="font-awesome"
+            size={24}
+            color={tintColor}
+          />
+        ),
+      },
+    },
     Home: {
       screen: HomeNavigator,
       navigationOptions: {
@@ -278,6 +317,7 @@ const MainNavigator = createDrawerNavigator(
     },
   },
   {
+    initialRouteName: "Home",
     drawerBackgroundColor: "#CEC8FF",
     contentComponent: CustomDrawerContentComponent,
   }
